3d989cdd2f3c7230f0f43fd5b151b9793a020a16
daltonhart <daltonvaren@gmail.com>
2019-08-20 16:58:12 -0700

    adds interaction with backend to display auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import Routes from './config/routes';
+import NavBar from './components/Layout/NavBar';
+import { API_URL } from './constants';
 
-function App() {
-  return <div className="App">Auth App</div>;
-}
+import './App.css';
 
-export default App;
+class App extends Component {
+  state = {
+    currentUser: localStorage.getItem('uid'),
+  };
+
+  setCurrentUser = (userId) => {
+    this.setState({ currentUser: userId });
+    localStorage.setItem('uid', userId);
+  };
+
+  logout = () => {
+    localStorage.removeItem('uid');
+    axios.post(`${API_URL}/auth/logout`, { withCredentials: true })
+      .then(res => {
+        console.log(res);
+        this.setState({ currentUser: null });
+        this.props.history.push('/login');
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <>
+        <NavBar currentUser={this.state.currentUser} logout={this.logout} />
+        <div className="container">
+          <Routes currentUser={this.state.currentUser} setCurrentUser={this.setCurrentUser} />
+        </div>
+      </>
+    );
+  };
+};
+
+export default withRouter(App);
